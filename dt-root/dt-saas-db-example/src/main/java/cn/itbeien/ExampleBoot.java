406e7e67962aae5af97ddc2fa4cf2c6576b4e92b
itbeien <baintong@aliyun.com>
2025-02-22 18:08:22 +0800

    dt-saas-db-example 如何在项目中集成saas-db

---
+++ dt-root/dt-saas-db-example/src/main/java/cn/itbeien/ExampleBoot.java
@@ -0,0 +1,19 @@
+package cn.itbeien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * 知识星球专享-多租户平台-SAAS DB组件
+ * Copyright© 2025 itbeien
+ */
+@SpringBootApplication
+public class ExampleBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBoot.class, args);
+    }
+}
---
+++ dt-root/dt-saas-db-example/src/main/java/cn/itbeien/example/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package cn.itbeien.example.config;
+
+import cn.itbeien.root.saas.util.StringUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+/**
+ * @author itbeien
+ * 项目网站：https://www.itbeien.cn
+ * 公众号：贝恩聊架构
+ * 全网同名，欢迎小伙伴们关注
+ * 知识星球专享-多租户平台-SAAS DB组件
+ * Copyright© 2025 itbeien
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new  RedisStandaloneConfiguration(host,port);
+        redisStandaloneConfiguration.setDatabase(database);
+
+        if (StringUtil.isEmpty(password)){
+            redisStandaloneConfiguration.setPassword(RedisPassword.none());
+        }else{
+            redisStandaloneConfiguration.setPassword(password);
+        }
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+}
